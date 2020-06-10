6b6fe8b92f40158654116e374647517093116e17
MECHREVO <huanghui0843@163.com>
2020-06-10 19:39:05 +0800

    ceshi

--- src/main/java/com/menzhen/controller/TestController.java
+++ src/main/java/com/menzhen/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @RequestMapping("index")
     public  String test(){
-        System.out.println("test");
+        System.out.println("test123");
         return "admin/index";
     }
 
